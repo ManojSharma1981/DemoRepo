a638eb5e163bdadb2066d9ec9a808d6d79700a86
Kumar Sharma <manoj.h.kumar.sharma@accenture.com>
2020-07-06 22:55:22 +0530

    First commit for Demo Project (Jenkins Integration Test)

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void test1() {
+		System.out.print("This is my first test");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.print("This is my second test");
+	}
+}
